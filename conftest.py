f50acf79bb05fcbe6af83d6b509bfe5c82ac374d
maazway <wahyu.priono@eydendigital.co.id>
2025-07-22 11:02:00 +0700

    update

--- conftest.py
+++ conftest.py
@@ -1,12 +1,18 @@
 import os
 import pytest
-from playwright.sync_api import sync_playwright
+
+pytest_plugins = ["pytest_playwright.plugin"]
 
 @pytest.fixture(scope="function")
 def page(playwright):
-    browser = playwright.chromium.launch(headless=os.getenv("HEADLESS", "true") == "true")
-    context = browser.new_context(user_agent=os.getenv("USER_AGENT", None))
+    headless = os.getenv("HEADLESS", "true").lower() == "true"
+    user_agent = os.getenv("USER_AGENT", None)
+
+    browser = playwright.chromium.launch(headless=headless)
+    context = browser.new_context(user_agent=user_agent)
     page = context.new_page()
+
     yield page
+
     context.close()
     browser.close()
