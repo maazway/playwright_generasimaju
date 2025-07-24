a44b8bf554e5beeed7d397533d08dc509333fbde
maazway <42727156+mazwaay@users.noreply.github.com>
2025-07-24 16:25:10 +0700

    add test case for kzb

--- pages/zat_besi_bunda.py
+++ pages/zat_besi_bunda.py
@@ -1,16 +1,33 @@
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
 
 class ZatBesiBunda:
     def __init__(self, page: Page):
         self.page = page
-        
-    def fill_nama_bunda(self):
-        input_nama_bunda = self.page.locator('input[placeholder="Masukkan nama bunda"]')
-        input_nama_bunda.fill("test wahyu hamil ticket")
-    def click_condition_sedang_hamil(self):
-        self.page.locator('xpath=//*[@id="formBunda"]/div[1]/div/div[1]/label/div/span').click()
-    def click_condition_sedang_menyusui(self):
-        self.page.locator('xpath=//*[@id="formBunda"]/div[1]/div/div[2]/label/div/span').click()
-    def choose_hpht(self):
-        self.page.locator('#lmp_date').click()
+        self.input_nama_bunda = page.locator('input[placeholder="Masukkan nama bunda"]')
+        self.radio_sedang_hamil = page.locator('xpath=//*[@id="formBunda"]/div[1]/div/div[1]/label/div/span')
+        self.input_tanggal_hpht = page.get_by_role("textbox", name="Contoh: 01-11-")
 
+    def fill_nama_bunda(self):
+        self.input_nama_bunda.fill("test wahyu hamil ticket")
+
+    def click_condition_sedang_hamil(self):
+        self.radio_sedang_hamil.click()
+
+    def klik_input_hpht(self):
+        self.input_tanggal_hpht.click()
+
+    def pilih_tanggal_hpht_feb_12_2025(self):
+        # Klik input tanggal
+        self.page.get_by_role("textbox", name="Contoh: 01-11-").click()
+
+        # Klik header bulan/tahun ("July 2025") untuk buka pilihan bulan
+        self.page.get_by_role("cell", name="2025").click()
+
+        # Pilih bulan Februari
+        self.page.get_by_text("Feb").click()
+
+        # Pilih tanggal 12
+        self.page.get_by_role("cell", name="12").click()
+
+    def klik_consent_checkbox(self):
+        self.page.get_by_role("checkbox", name="Dengan ini bersedia untuk").click()
--- tests/zat_besi_bunda/test_all_zat_besi_bunda.py
+++ tests/zat_besi_bunda/test_all_zat_besi_bunda.py
@@ -3,18 +3,19 @@ from pages.zat_besi_bunda import ZatBesiBunda
 from helpers.login_helpers import login_valid
 
 def test_zat_besi_bunda(page):
-    # Gunakan reusable login
+    # Login
     login_valid(page)
 
-    # Interaksi setelah login
+    # Navigasi ke halaman Zat Besi Bunda
     home_page = HomePage(page)
     home_page.hover_kalkulator_menu()
     home_page.click_submenu_bunda()
 
-    # Inisialisasi halaman ZatBesiBunda
+    # Isi form
     zat_besi_bunda_page = ZatBesiBunda(page)
     zat_besi_bunda_page.fill_nama_bunda()
     zat_besi_bunda_page.click_condition_sedang_hamil()
-
-
-    page.wait_for_timeout(10000)
+    zat_besi_bunda_page.pilih_tanggal_hpht_feb_12_2025()
+    zat_besi_bunda_page.klik_consent_checkbox()
+    # Delay untuk observasi manual (opsional - bisa dihapus di production)
+    page.wait_for_timeout(5000)
