ebc69782c3a0b78ed7f178d8f38b3b3a83066213
maazway <42727156+mazwaay@users.noreply.github.com>
2025-07-28 13:38:12 +0700

    change name of test function

--- tests/zat_besi_bunda/test_all_zat_besi_bunda.py
+++ tests/zat_besi_bunda/test_all_zat_besi_bunda.py
@@ -2,7 +2,7 @@ from pages.home_page import HomePage
 from pages.zat_besi_bunda import ZatBesiBunda
 from helpers.login_helpers import login_valid
 
-def test_zat_besi_bunda(page):
+def test_pragnant_untick_login_berisiko(page):
     # Login
     login_valid(page)
     # Navigasi ke halaman Zat Besi Bunda
@@ -36,3 +36,4 @@ def test_zat_besi_bunda(page):
 
     # Delay untuk observasi manual (opsional - bisa dihapus di production)
     page.wait_for_timeout(10000) # delay 10 detik
+
